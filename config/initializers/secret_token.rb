a343b047e1f37a8e8105681453028a60486567bf
Charles Ellis <hamled@hamled.name>
2013-09-15 13:54:43 -0700

    Create rails project skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsTutorial::Application.config.secret_key_base = '16d80a219f96c2ea069554322763aec0cb412cd845c00816e44cbdce0681af6f0096a77c50d88e5aab66a474e54016e70ee206a7222998ef8f0179aaa63ee24e'

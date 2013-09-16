e57fe06825e8a2c1ebd1c27abd27df7f59c2f359
Charles Ellis <hamled@hamled.name>
2013-09-16 02:20:23 -0700

    Create rails skeleton without unit tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsTutorial::Application.config.secret_key_base = 'a52d89480195f270bfe02d70ca0658954e10f21fdd96ca1a604507e05666bbe6f78f3bb0f147652ac251453c6e8dca55130419bd8957f904ad6496375fa6c145'

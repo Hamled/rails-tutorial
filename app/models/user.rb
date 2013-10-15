a40c4eaa76cda90174d802844641c95bc4932f71
Charles Ellis <hamled@hamled.name>
2013-10-14 22:43:06 -0700

    Add validations for User fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+  NAME_MAXLEN = 50
+  validates :name, presence: true, length: { maximum: NAME_MAXLEN }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
